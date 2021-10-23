b0f34a34dbf26ec4e9d220e2f86841c13af875f7
Brad Conklin <bconklin@ualberta.ca>
2021-10-23 11:56:40 -0600

    Show All Habits Outline

--- app/src/main/java/com/example/spacejuice/Member.java
+++ app/src/main/java/com/example/spacejuice/Member.java
@@ -1,4 +1,27 @@
 package com.example.spacejuice;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 public class Member {
+   private static Member instance;
+   public static ArrayAdapter<Habit> habitAdapter;
+   public static ArrayList<Habit> habitList;
+
+   private Member(Context activity, int layout) {
+      habitList = new ArrayList<>();
+      if (habitAdapter == null) {
+         habitAdapter = new ArrayAdapter<>(activity, layout, habitList);
+      }
+   }
+
+
+   public static Member getInstance(Context activity, int layout) {
+      if (instance == null) {
+         instance = new Member(activity, layout);
+      }
+      return instance;
+   }
 }
--- app/src/main/java/com/example/spacejuice/activity/AllHabitsActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/AllHabitsActivity.java
@@ -1,7 +1,34 @@
 package com.example.spacejuice.activity;
 
-public class AllHabitsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spacejuice.Habit;
+import com.example.spacejuice.Member;
+import com.example.spacejuice.R;
+
+public class AllHabitsActivity extends AppCompatActivity {
    /*
    This Activity is used to view all of my habits
     */
+
+   public ListView habitList;
+   public static ArrayAdapter<Habit> habitAdapter;
+   public Member member;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.my_habit_list);
+      habitList = findViewById(R.id.my_habit_list);
+      this.member = member.getInstance(this, R.layout.habit_content);
+      habitList.setAdapter(habitAdapter);
+   }
+
 }
