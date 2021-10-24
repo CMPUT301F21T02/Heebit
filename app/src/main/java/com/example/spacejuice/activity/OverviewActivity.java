0a8031f5e36f45feb14413004c4ba6e4882d905c
hsprabhakar <saurab.harish@gmail.com>
2021-10-24 16:19:12 -0600

    Created Overview.java

--- app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
@@ -1,10 +1,23 @@
 package com.example.spacejuice.activity;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spacejuice.R;
 
 public class OverviewActivity extends AppCompatActivity {
    /*
    This Activity is used my main page which shows an overlay of today's habits
    and various menus
     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.today_habits);
+
+    }
 }
