42f6dba9cc663b1b366b0746e9eff89a23e8ed51
hsprabhakar <saurab.harish@gmail.com>
2021-10-25 17:47:49 -0600

    added listadapter for overviewactivity

--- app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
@@ -4,18 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.spacejuice.Habit;
 import com.example.spacejuice.R;
+
+import java.util.ArrayList;
 
 public class OverviewActivity extends AppCompatActivity {
 
     Button go_to_all_habits_button;
     Button go_to_add_habit_button;
     ImageView go_to_profile_imageView;
+    public ListView today_habit_list;
+    public static ArrayAdapter<Habit> habitAdapter;
+
    /*
    This Activity is used my main page which shows an overlay of today's habits
    and various menus
@@ -27,7 +35,15 @@ public class OverviewActivity extends AppCompatActivity {
         setContentView(R.layout.today_habits);
 
         go_to_all_habits_button = (Button) findViewById(R.id.all_habits_button);
+        today_habit_list = findViewById(R.id.overview_habit_listview);
 
+        ArrayList<Habit> today_habit_items = new ArrayList<>();
+
+        today_habit_items.add(new Habit("test habit #1", "reason #1", Habit.Indicator.EMPTY));
+        today_habit_items.add(new Habit("test habit #2", "reason #2", Habit.Indicator.BRONZE));
+        today_habit_items.add(new Habit("test habit #3", "reason #3", Habit.Indicator.SILVER));
+        today_habit_items.add(new Habit("test habit #4", "reason #4", Habit.Indicator.GOLD));
+        today_habit_list.setAdapter(new HabitListAdapter(this, R.layout.overview_habit_content,today_habit_items ));
 
 
         go_to_all_habits_button.setOnClickListener(new View.OnClickListener() {
