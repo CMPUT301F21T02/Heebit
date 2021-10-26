b3d6e96af22cd8c359b334827de6f01580feedf5
hsprabhakar <saurab.harish@gmail.com>
2021-10-26 17:27:30 -0600

    addedoverviewcontent adapter

--- app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/OverviewActivity.java
@@ -3,12 +3,17 @@ package com.example.spacejuice.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.spacejuice.Habit;
 import com.example.spacejuice.R;
+
+import java.util.ArrayList;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -16,11 +21,12 @@ public class OverviewActivity extends AppCompatActivity {
     Button go_to_add_habit_button;
     ImageView go_to_profile_imageView;
     ImageView go_to_addHabit_imageView;
-   /*
-   This Activity is used my main page which shows an overlay of today's habits
-   and various menus
-    */
-
+    /*
+    This Activity is used my main page which shows an overlay of today's habits
+    and various menus
+     */
+    public ListView today_habit_list;
+    public static ArrayAdapter<Habit> habitAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,15 @@ public class OverviewActivity extends AppCompatActivity {
         go_to_addHabit_imageView = findViewById(R.id.add_habit_image_view);
 
         go_to_all_habits_button = (Button) findViewById(R.id.all_habits_button);
+
+        today_habit_list = findViewById(R.id.overview_habit_listview);
+        ArrayList<Habit> today_habit_items = new ArrayList<>();
+
+        today_habit_items.add(new Habit("test habit #1", "reason #1", Habit.Indicator.LEVEL00));
+        today_habit_items.add(new Habit("test habit #2", "reason #2", Habit.Indicator.LEVEL00));
+        today_habit_items.add(new Habit("test habit #3", "reason #3", Habit.Indicator.LEVELNEW));
+        today_habit_items.add(new Habit("test habit #4", "reason #4", Habit.Indicator.LEVELNEW));
+        today_habit_list.setAdapter(new HabitListAdapter(this, R.layout.overview_habit_content,today_habit_items ));
 
 
 
