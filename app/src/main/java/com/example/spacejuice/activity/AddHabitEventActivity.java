fdbfd45190b95ca10a827e81a17f161a9a221a6e
Brad Conklin <bconklin@ualberta.ca>
2021-10-27 17:13:43 -0600

    can view habit event page

--- app/src/main/java/com/example/spacejuice/activity/AddHabitEventActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/AddHabitEventActivity.java
@@ -1,9 +1,34 @@
 package com.example.spacejuice.activity;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spacejuice.R;
 
 public class AddHabitEventActivity extends AppCompatActivity {
       /*
    This Activity is used to add and enter the details of a new habit event
     */
+      public ImageButton back_button;
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         Log.d("debugInfo", "My Profile View Created from MyProfileActivity.java");
+         setContentView(R.layout.event_add);
+
+         back_button = findViewById(R.id.backButtonEA);
+
+         back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+
+         });
+      }
 }
