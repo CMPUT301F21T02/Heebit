32a1fe489b89fc27f352c4a4ce20c3ddf90ff7ab
qpdpQ <46284272+qpdpQ@users.noreply.github.com>
2021-10-29 10:16:50 -0600

    connected to firebase
    still have bug

--- app/src/main/java/com/example/spacejuice/activity/LoginActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/LoginActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.spacejuice.MainActivity;
+import com.example.spacejuice.Member;
 import com.example.spacejuice.R;
 import com.example.spacejuice.controller.LoginController;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
    /*
@@ -23,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText UserNameET;
     EditText PassWordET;
     LoginController loginController;
+    public ArrayList<Member> MemberDataList = new ArrayList<Member>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +36,14 @@ public class LoginActivity extends AppCompatActivity {
         signUpButton = findViewById(R.id.signupButton);
         UserNameET = findViewById(R.id.userName);
         PassWordET = findViewById(R.id.editTextTextPassword);
+        TextView textView = findViewById(R.id.textView6);
         loginController = new LoginController();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean success;
-                success = loginController.login(UserNameET.getText().toString(),
+                MemberDataList = loginController.login(UserNameET.getText().toString(),
                         PassWordET.getText().toString());
-                if (success){
+                if (!MemberDataList.isEmpty()){
                     Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT);
                     // Feel free to change to whatever but setting to MainActivity makes it super buggy lmfao - harish
                     Intent intent = new Intent(LoginActivity.this, OverviewActivity.class);
@@ -54,14 +59,23 @@ public class LoginActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginController.signUp(UserNameET.getText().toString() ,
+                MemberDataList = loginController.signUp(UserNameET.getText().toString() ,
                         PassWordET.getText().toString());
-                Toast.makeText(LoginActivity.this, "Signed Up!",
-                        Toast.LENGTH_SHORT);
-                Intent intent = new Intent(LoginActivity.this, OverviewActivity.class);
-                startActivity(intent);
+                if( !MemberDataList.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Signed Up!",
+                            Toast.LENGTH_SHORT);
+                    UserNameET.setText("");
+                    PassWordET.setText("");
+                    Intent intent = new Intent(LoginActivity.this, OverviewActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Username has been used",
+                            Toast.LENGTH_SHORT);
+                    PassWordET.setText(""); // clear the Text
+                    textView.setVisibility(View.VISIBLE);
+                }
             }
-
         });
     }
 }
