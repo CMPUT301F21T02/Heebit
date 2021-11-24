de3c7cc488c856be263657dce231970bdeb55ca0
JoshuaN18 <beomyoon@ualberta.ca>
2021-11-23 20:42:42 -0700

    Setup test case

--- app/src/androidTest/java/com/example/spacejuice/MainActivityTest.java
+++ app/src/androidTest/java/com/example/spacejuice/MainActivityTest.java
@@ -1,61 +1,49 @@
 package com.example.spacejuice;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertTrue;
 
-import static java.util.regex.Pattern.matches;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.widget.EditText;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.intent.Intents;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.spacejuice.activity.AddHabitActivity;
-import com.example.spacejuice.activity.AllHabitsActivity;
 import com.example.spacejuice.activity.LoginActivity;
-import com.example.spacejuice.activity.OverviewActivity;
 import com.example.spacejuice.activity.WelcomeActivity;
 import com.robotium.solo.Solo;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
-@LargeTest
 public class MainActivityTest {
 
+    private Solo solo;
+
     @Rule
-    public ActivityTestRule<MainActivity> activityRule =
-            new ActivityTestRule<>(MainActivity.class, true, false);
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    @Before
+    public void setUp(){
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start(){
+        Activity activity = rule.getActivity();
+    }
 
     @Test
     public void login(){
-        Intents.init();
-        activityRule.launchActivity(new Intent());
-        intended(hasComponent(WelcomeActivity.class.getName()));
+        solo.clickOnButton("Log in");
+        assertTrue(solo.waitForActivity(LoginActivity.class));
+        solo.enterText((EditText) solo.getView(R.id.userName), "Josh4");
+        solo.enterText((EditText) solo.getView(R.id.editTextTextPassword), "12345");
+        solo.clickOnButton("Login");
 
-        Espresso.onView(withId(R.id.welcome_loginButton)).perform(click());
-        intended(hasComponent(LoginActivity.class.getName()));
-        Espresso.onView(withId(R.id.userName)).perform(typeText("Josh"));
-        Espresso.onView(withId(R.id.editTextTextPassword)).perform(typeText("12345"));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(withId(R.id.loginButton)).perform(click());
-        Intents.release();
+
     }
 
 }
