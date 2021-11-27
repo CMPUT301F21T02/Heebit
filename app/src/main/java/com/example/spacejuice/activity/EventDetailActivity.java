cc1e22ff7d7755b2fa20fa42ae485d6f4aa77401
qpdpQ <liyuchen223@gmail.com>
2021-11-26 23:58:42 -0700

    rebuild the eventDetail

--- app/src/main/java/com/example/spacejuice/activity/EventDetailActivity.java
+++ app/src/main/java/com/example/spacejuice/activity/EventDetailActivity.java
@@ -1,18 +1,27 @@
 package com.example.spacejuice.activity;
 
+import static android.content.ContentValues.TAG;
 import static com.example.spacejuice.controller.HabitEventController.getDocumentIdString;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.spacejuice.Habit;
 import com.example.spacejuice.HabitEvent;
@@ -20,33 +29,55 @@ import com.example.spacejuice.MainActivity;
 import com.example.spacejuice.R;
 import com.example.spacejuice.controller.HabitController;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 //todo add call back
 public class EventDetailActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE = 1;
     Button back;
-    Button upload;
+    Button confirm;
     Button editImage;
+    Button takePhoto;
     EditText editText;
     ImageView imageView;
+    ProgressBar progressBar;
+    private Button uploadImage;
+    private Bitmap photo;
+    Uri imageUri;
     int eventId;
     int habitId;
     int event;
     String habit;
     String U;
+    private StorageTask uploadTask;
+    private StorageReference storageReference;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("debugInfo", "onCrate");
         setContentView(R.layout.activity_event_detail);
+        uploadImage = findViewById(R.id.edit_upload);
         back = findViewById(R.id.edit_back);
-        upload = findViewById(R.id.upload);
-        editImage = findViewById(R.id.edit_photo);
+        confirm = findViewById(R.id.edit_confirm);
+        editImage = findViewById(R.id.choose_image);
+        takePhoto = findViewById(R.id.take_photo);
         editText = findViewById(R.id.edit_editText);
         imageView = findViewById(R.id.edit_imageView);
+        progressBar = findViewById(R.id.edit_progress_bar);
         habit = getIntent().getExtras().getString("habit");
         habitId = getIntent().getExtras().getInt("habitId");
         event = getIntent().getExtras().getInt("event");
@@ -69,7 +100,22 @@ public class EventDetailActivity extends AppCompatActivity {
             }
         });
 
-        upload.setOnClickListener(new View.OnClickListener() {
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(uploadTask != null && uploadTask.isInProgress()){
+                    Toast.makeText(EventDetailActivity.this,"Upload in progress",Toast.LENGTH_SHORT).show();
+                }
+                else if(imageUri == null){
+                    Toast.makeText(EventDetailActivity.this, "No file selected", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    uploadFile();
+                }
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //e.setDescription(editText.getText().toString());
@@ -127,12 +173,110 @@ public class EventDetailActivity extends AppCompatActivity {
         editImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent habitIntent = new Intent(EventDetailActivity.this, UploadImageActivity.class);
-                habitIntent.putExtra("habit", habit);
-                startActivity(habitIntent);
+                openFileChooser();
             }
         });
     }
+
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            Log.d(TAG, "The start event");
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+
+        if(requestCode == 100 && resultCode == RESULT_OK)
+        {
+            Log.d(TAG, "The start photo event");
+            photo = (Bitmap) data.getExtras().get("data");
+            imageUri = getImageUri(EventDetailActivity.this, photo);
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile(){
+        if(imageUri != null){
+            DocumentReference docReference = FirebaseFirestore.getInstance().collection("Members")
+                    .document(MainActivity.getUser().getMemberName())
+                    .collection("Habits").document(getIntent().getExtras().getString("habit"));
+            StorageReference fileReference = storageReference.child(
+                    MainActivity.getUser().getMemberName()+ getIntent().getExtras().getString("habit") + String.valueOf(System.currentTimeMillis())
+                            +"."+ getFileExtension(imageUri));
+            uploadTask = fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            }, 500);
+                            Toast.makeText(EventDetailActivity.this, "Upload successful"
+                                    , Toast.LENGTH_LONG).show();
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String url = uri.toString();
+                                    Map<String,Object> Duri = new HashMap<>();
+                                    Duri.put("url", url);
+                                    docReference.update(Duri)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    Log.d("message", "URL has been added successfully");
+                                                    //Picasso.get().load(imageUri).into(imageView);
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EventDetailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            progressBar.setProgress((int) progress);
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
 
 }
